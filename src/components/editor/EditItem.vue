5f6611932592e92acb18ae8a32638525cdb92afe
李昊 <1097261154@qq.com>
2022-08-01 11:53:36 +0800

    修改默认功能，完善文档编辑

--- src/components/editor/EditItem.vue
+++ src/components/editor/EditItem.vue
@@ -5,12 +5,13 @@
 <script setup>
 import Vditor from 'vditor';
 import 'vditor/dist/index.css';
+import defaultValue from '@/config/editor.js';
 const vditor = ref();
 onMounted(() => {
   vditor.value = new Vditor('vditor', {
     after: () => {
       // vditor.value is a instance of Vditor now and thus can be safely used here
-      vditor.value.setValue('HELLO');
+      vditor.value.setValue(defaultValue);
     },
   });
 });
