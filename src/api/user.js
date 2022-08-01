db41d0bd8ca7a3e3676d41caa45a292f282cf573
your name <your email address>
2022-08-01 17:40:34 +0800

    team create.

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,4 @@
-import http from '@/http/request.js'
+import service from '@/http/request.js'
 
 const url = {
     getUserList: '/user/list',
@@ -6,7 +6,7 @@ const url = {
 
 export class User {
     static async getUserList(data) {
-        return http(url.getUserList, {
+        return service(url.getUserList, {
           method: 'get',
           responseType: 'json',
           data: data
