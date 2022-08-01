fc8450c3884e19c7c27143ff7ed9ffb058769d8f
hjc-owo <2535199657@qq.com>
2022-08-01 17:00:40 +0800

    初步的登录注册（

--- src/components/login/LoginItem.vue
+++ src/components/login/LoginItem.vue
@@ -1,11 +1,87 @@
 <template>
-  
+  <el-form
+    ref="loginFormRef"
+    :model="account"
+    status-icon
+    :rules="rules"
+    label-width="120px"
+    class="demo-ruleForm"
+    label-position="top"
+    size="large"
+  >
+    <el-form-item label="邮箱" prop="email">
+      <el-input v-model="account.email" />
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input v-model="account.password" type="password" autocomplete="off" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(loginFormRef)"
+        >登录</el-button
+      >
+      <el-button @click="resetForm(loginFormRef)">重置内容</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import type { FormInstance } from "element-plus";
+import { useStateStore } from "../../stores/state";
 
+const loginFormRef = ref<FormInstance>();
+const stateStore = useStateStore();
+
+const validateEmail = function (rule, value, callback) {
+  if (value === "") {
+    return callback(new Error("邮箱不能为空！"));
+  } else {
+    const emailPattern = /^\w+@[0-9a-z]+\.[a-z]+$/;
+    if (!emailPattern.test(value)) {
+      callback(new Error("请输入正确的邮箱格式！"));
+    }
+    callback();
+  }
+};
+
+const validatePassword = function (rule, value, callback) {
+  if (value === "") {
+    callback(new Error("请输入密码"));
+  } else {
+    callback();
+  }
+};
+
+const account = reactive({
+  email: "",
+  password: "",
+});
+
+const rules = reactive({
+  email: [{ validator: validateEmail, trigger: "blur" }],
+  password: [{ validator: validatePassword, trigger: "blur" }],
+});
+
+const submitForm = function (formEl) {
+  if (!formEl) return;
+  formEl.validate(function (valid) {
+    if (valid) {
+      console.log("submit!");
+      var payload = new FormData();
+      payload.append("email", account.email);
+      payload.append("password", account.password);
+      stateStore.loginAction(payload);
+    } else {
+      console.log("error submit!");
+      return false;
+    }
+  });
+};
+
+const resetForm = function (formEl) {
+  if (!formEl) return;
+  formEl.resetFields();
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
