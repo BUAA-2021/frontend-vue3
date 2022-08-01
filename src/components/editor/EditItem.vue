e456e1e8debdee1b6de72467bc3184223feebec7
李昊 <1097261154@qq.com>
2022-08-01 13:26:37 +0800

    添加输入回调，进行Vditor基本配置

--- src/components/editor/EditItem.vue
+++ src/components/editor/EditItem.vue
@@ -1,23 +1,40 @@
 <template>
-  <div id = "vditor"></div>
+  <div id="vditor" style="margin: 0 auto"></div>
 </template>
 
 <script setup>
-import Vditor from 'vditor';
-import 'vditor/dist/index.css';
-import defaultValue from '@/config/editor.js';
+import Vditor from "vditor";
+import "vditor/dist/index.css";
+import defaultValue from "@/config/editor.js";
+import { useStorage } from "@vueuse/core";
 const vditor = ref();
+let editorValue = ref("");
 onMounted(() => {
-  vditor.value = new Vditor('vditor', {
+  vditor.value = new Vditor("vditor", {
+    width: "60%",
+    preview: {
+      hljs: {
+        lineNumber: true,
+      },
+    },
+    toolbarConfig: {
+      pin: true,
+    },
+    outline: {
+      enable: true,
+    },
     after: () => {
-      // vditor.value is a instance of Vditor now and thus can be safely used here
-      vditor.value.setValue(defaultValue);
+      editorValue = useStorage("vditorvditor");
+      if (!editorValue.value.trim()) {
+        editorValue.value = defaultValue;
+      }
+    },
+    input: (md) => {
+      console.log("EEE=INPUT", md);
     },
   });
 });
-
 </script>
 
 <style>
-
 </style>
