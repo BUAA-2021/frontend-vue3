5983ab73cd9183d2566d288e2705e10e5595bac0
李昊 <1097261154@qq.com>
2022-08-02 15:19:29 +0800

    JSX！！！

--- src/views/example.vue
+++ src/views/example.vue
@@ -1,30 +1,7 @@
-<template>
-  <DDR onResize={this.handleResize} onRotate={this.handleRotate} onDrag={this.handleDrag} value={this.transform}>
-        <div style="width:100%;height:100%">child</div>
-    </DDR>
-</template>
-
-<script>
-import DDR from 'yoyoo-ddr'
-import 'yoyoo-ddr/dist/yoyoo-ddr.css'
-export default {
-data() {
-    return {
-      transform: { x: 100, y: 100, width: 100, height: 100, rotation: 0 },
-    }
-  },
-  methods: {
-    handleDrag(event, transform) {
-      this.transform = transform
-    },
-    handleResize(event, transform) {
-      this.transform = transform
-    },
-    handleRotate(event, transform) {
-      this.transform = transform
-    },
-  },
-}
+<script lang="jsx">
+export default defineComponent({
+   render() { return <div>Test</div> }
+})
 </script>
 
 <style>
