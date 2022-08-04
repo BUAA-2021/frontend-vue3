bdc6b545c2851577ab13ddac7d086412f98aa465
李昊 <1097261154@qq.com>
2022-08-04 09:47:44 +0800

    template

--- src/examples/vseditor/components.vue
+++ src/examples/vseditor/components.vue
@@ -1,104 +1,104 @@
-<script lang="jsx">
-import Logo from '@/assets/logo.png'
+<template>
+  <div class="vs-components">
+    <template 
+    v-for="(item, index) in components"
+    :key="index"
+    >
+    <div @dragstart="(e) => this.handleDrag(e, item)" draggable="true" class="components-item">
+    <img class="components-icon" :src="getImageUrl(item)" />
+    {{item.name}}
+    </div>
+    </template>
+  </div>
+</template>
+<script>
+import Logo from "@/assets/logo.png";
 export default {
   data() {
     return {
       components: [
         {
-          type: 'label',
-          value: 'Hi~我是一段文本',
-          name: '文本框',
+          type: "label",
+          value: "Hi~我是一段文本",
+          name: "文本框",
           width: 150,
           height: 30,
         },
         {
-          type: 'input',
-          value: '输入',
-          inputType: 'input',
-          name: '输入框',
+          type: "input",
+          value: "输入",
+          inputType: "input",
+          name: "输入框",
           width: 150,
           height: 30,
         },
         {
-          type: 'select',
+          type: "select",
           options: [],
-          name: '复选框',
+          name: "复选框",
           width: 150,
           height: 30,
         },
         {
-          type: 'img',
-          name: '图片',
+          type: "img",
+          name: "图片",
           url: Logo,
           width: 100,
           height: 100,
         },
         {
-          type: 'input',
-          inputType: 'checkbox',
-          name: '多选框',
-          label: '多选',
+          type: "input",
+          inputType: "checkbox",
+          name: "多选框",
+          label: "多选",
           width: 100,
           height: 30,
         },
         {
-          type: 'input',
-          inputType: 'radio',
-          name: '单选框',
-          label: '单选',
+          type: "input",
+          inputType: "radio",
+          name: "单选框",
+          label: "单选",
           width: 100,
           height: 30,
         },
         {
-          type: 'input',
-          inputType: 'button',
-          name: '按钮',
-          value: 'Button',
+          type: "input",
+          inputType: "button",
+          name: "按钮",
+          value: "Button",
           width: 100,
           height: 30,
         },
         {
-          type: 'input',
-          inputType: 'date',
-          name: '日期',
-          value: '2021-10-10',
+          type: "input",
+          inputType: "date",
+          name: "日期",
+          value: "2021-10-10",
           width: 100,
           height: 30,
         },
         {
-          type: 'container',
-          name: '容器',
+          type: "container",
+          name: "容器",
           width: 200,
           height: 200,
           children: [],
         },
       ],
-    }
+    };
   },
   methods: {
     handleDrag(e, item) {
-      e.dataTransfer.dropEffect = 'copy'
-      e.dataTransfer.setData('text/component', JSON.stringify(item))
+      e.dataTransfer.dropEffect = "copy";
+      e.dataTransfer.setData("text/component", JSON.stringify(item));
     },
-    getImageUrl(item){
-      return new URL(`/${item.inputType||item.type}.png`, import.meta.url).href
-    }
+    getImageUrl(item) {
+      return new URL(`/${item.inputType || item.type}.png`, import.meta.url)
+        .href;
+    },
   },
-  render() {
-    return (
-      <div class="vs-components">
-        {this.components.map((item, index) => {
-          return (
-            <div ondragstart={(e) => this.handleDrag(e, item)} draggable="true" class="components-item" key={index}>
-              <img class="components-icon" src={this.getImageUrl(item)} />
-              {item.name}
-            </div>
-          )
-        })}
-      </div>
-    )
-  },
-}
+};
 </script>
 
 <style lang="less" scoped>
