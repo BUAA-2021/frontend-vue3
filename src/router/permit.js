e588ed29f6ea59379368f6036566b7164bdd71fb
李昊 <1097261154@qq.com>
2022-08-05 00:34:44 +0800

    路由守卫更改

--- src/router/permit.js
+++ src/router/permit.js
@@ -12,16 +12,15 @@ router.beforeEach(async (to, from,next) => {
             type: 'warning',
             message: '请先进行登录'
         })
-        return ({name:'login'})
+        next({ name:'login' })
     }
     else if(token!=null
         && (to.name==='login'||to.name=='register')){
-            next('/team/create');
             ElMessage({
                 type: 'warning',
                 message: '您已经登录'
             })
-            return false;
+            next({path:'/team/create'})
         }
-    
+    else next();
 })
