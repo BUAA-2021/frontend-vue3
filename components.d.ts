642ddffc786732cb9b54d1bd4426a9621b0b116b
李昊 <1097261154@qq.com>
2022-08-05 11:01:57 +0800

    MERGE

--- components.d.ts
+++ components.d.ts
@@ -11,7 +11,6 @@ declare module '@vue/runtime-core' {
     EditItem: typeof import('./src/components/editor/EditItem.vue')['default']
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElCard: typeof import('element-plus/es')['ElCard']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDialog: typeof import('element-plus/es')['ElDialog']
@@ -21,7 +20,6 @@ declare module '@vue/runtime-core' {
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
@@ -43,6 +41,5 @@ declare module '@vue/runtime-core' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SideBar: typeof import('./src/components/common/SideBar.vue')['default']
-    '副本': typeof import('./src/components/editor/EditItem - 副本.vue')['default']
   }
 }
