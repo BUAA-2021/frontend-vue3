92e88b712dcc4ac09cab959192f2ea28fc44dfea
woarthur <78649056+woarthur@users.noreply.github.com>
2022-08-05 10:57:13 +0800

    修改编辑器页面

--- src/components/editor/NewEditor.vue
+++ src/components/editor/NewEditor.vue
@@ -1,18 +1,38 @@
 <template>
-  <div style="width:70%; margin: 0 auto;background: white;margin-top: 3vh;">
-    <div class="editor" v-if="editor">
-      <menu-bar class="editor__header" :editor="editor" />
-      <editor-content class="editor__content" :editor="editor" />
-      <div class="editor__footer">
-        <div :class="`editor__status editor__status--${status}`">
-          <template v-if="status === 'connected'">
-            {{ editor.storage.collaborationCursor.users.length }} 位用户在编辑{{ room }}
-          </template>
-          <template v-else> 离线 </template>
+  <el-container class="wrap">
+    <el-main>
+      <el-row>
+        <div class="title">
+          <h2>在线文档 : {{ route.query.name }}</h2>
         </div>
-      </div>
-    </div>
-  </div>
+      </el-row>
+      <el-row>
+        <div
+          style="
+            width: 70%;
+            margin: 0 auto;
+            background: white;
+            margin-top: 1.5vh;
+          "
+          class="editor1"
+        >
+          <div class="editor" v-if="editor">
+            <menu-bar class="editor__header" :editor="editor" />
+            <editor-content class="editor__content" :editor="editor" />
+            <div class="editor__footer">
+              <div :class="`editor__status editor__status--${status}`">
+                <template v-if="status === 'connected'">
+                  {{ editor.storage.collaborationCursor.users.length }}
+                  位用户在编辑{{ room }}
+                </template>
+                <template v-else> 离线 </template>
+              </div>
+            </div>
+          </div>
+        </div>
+      </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script setup>
@@ -34,11 +54,11 @@ const route = useRoute();
 const getRandomElement = (list) => {
   return list[Math.floor(Math.random() * list.length)];
 };
-const currentUser = (state.userRealname) || (state.userNickname);
+const currentUser = state.userRealname || state.userNickname;
 const provider = ref(null);
 const editor = ref(null);
 const status = ref("connecting");
-const room = ref(route.query.name||route.params.id);
+const room = ref(route.query.name || route.params.id);
 
 onMounted(() => {
   const ydoc = new Y.Doc();
@@ -126,3 +146,19 @@ onUnmounted(() => {
   provider.value.destroy();
 });
 </script>
+
+<style scoped>
+.wrap {
+  background-color: #7b91cb;
+  width: 100%;
+  height: 100%;
+  opacity: 0.8;
+  backdrop-filter: blur(20px);
+  transition: 0.3s;
+}
+
+.title {
+  margin-left: 16%;
+  color: #f8fefc;
+}
+</style>
